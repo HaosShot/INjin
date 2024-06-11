80e71d587ff4e4e9c8e6701635bd527d02675f53
Admin <Admin@DESKTOP-QFTHS3A>
2024-06-11 11:08:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,22 @@
+﻿#include <gtest/gtest.h>
+#include <vector>
+
+std::vector<int> increaseVector(std::vector<int> vec, int n) {
+    for (int& elem : vec) {
+        elem += n;
+    }
+    return vec;
+}
+
+TEST(IncreaseVectorTest, SimpleTest) {
+    
+    std::vector<int> input = { 1, 2, 3 };
+
+    std::vector<int> expected = { 4, 5, 6 };
+
+    std::vector<int> result = increaseVector(input, 3);
+
+    EXPECT_EQ(result, expected);
+}
+
+
